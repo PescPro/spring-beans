4b0958c98822729eb1b266c438cbb9d7374b32d2
PescPro <471844223@qq.com>
2019-01-04 12:45:50 +0800

    git 提交

--- src/test/java/com/pesc/study/spring/beans/AppTest.java
+++ src/test/java/com/pesc/study/spring/beans/AppTest.java
@@ -1,36 +1,31 @@
 package com.pesc.study.spring.beans;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:/spring-beans-xml.xml"})
+public class AppTest
 {
     /**
      * Create the test case
-     *
-     * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTest(  )
     {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+        super();
     }
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
